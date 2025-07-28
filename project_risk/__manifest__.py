10686fe5a36f8de1c368f4dbf10615f0421c9e0f
NachoDuonex <ignacio.gonzalez@duonex.com>
2025-07-28 18:04:02 +0200

    Update __manifest__.py

--- project_risk/__manifest__.py
+++ project_risk/__manifest__.py
@@ -2,10 +2,10 @@
     "name": "Project Risk",
     "summary": "MOR risk management method",
     "author": "Onestein, Odoo Community Association (OCA)",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "website": "https://github.com/OCA/project",
     "category": "Project Management",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "depends": ["project"],
     "data": [
         "security/ir_model_access.xml",
